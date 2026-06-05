e28e759ccd470919554213f7de9e1e3a51179c3a
excalategit <oloruntoba@excalate.pl>
2026-06-05 13:30:07 +0200

    Comment refined for fact table enrichment explanation.

--- 02 DL to DW/04 data pipeline 2.py
+++ 02 DL to DW/04 data pipeline 2.py
@@ -363,7 +363,8 @@ def load_fact_sale_product():
 
     try:
         # The products column is an ARRAY, to flatten it requires UNNEST before it can be used in a query.
-        # Also, to preserve the fact table during enrichment with dim surrogate keys an outer join is used.
+        # Also, an outer join is used at the fact table enrichment (with dim table surrogate keys) step in
+        # order to preserve the fact table i.e. ensuring that all fact table data is preserved no matter what.
         fact_sale_product_dataset = '''
         SELECT f.sale_key, p.product_key, pp.price, flat.quantity, pp.price * flat.quantity as total_sale, 
         pp.rating.count as stock
